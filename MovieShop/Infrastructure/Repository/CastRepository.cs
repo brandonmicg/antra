a4c4e5114a0c098352e5e3a47afb896c36f5a4ad
Brandon Girard <branmic11@gmail.com>
2022-06-13 19:53:07 -0400

    added MovieShop hw

---
+++ MovieShop/Infrastructure/Repository/CastRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApplicationCore.Contracts;
+
+namespace Infrastructure.Repository
+{
+    public class CastRepository : ICastRepository
+    {
+    }
+}
